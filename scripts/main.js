0a667e62c2ae0a8b9b75f72b417ab8766ac098c0
Nikhil Gupta <nikhil@podkart.com>
2015-06-14 11:59:54 +0530

    trying to revert to previous version

---
+++ scripts/main.js
@@ -0,0 +1,64 @@
+// Academics
+var gradText = document.getElementById('academics_text');
+gradText.style.display = "none";
+var grad_tile = document.getElementById('academics');
+
+grad_tile.onmouseover = function() {
+  gradText.style.display = "block";
+}
+
+grad_tile.onmouseout = function() {
+  gradText.style.display = "none";
+}
+
+// Experience
+var expText = document.getElementById('exp_text');
+expText.style.display = "none";
+var exp_tile = document.getElementById('exp');
+
+exp_tile.onmouseover = function() {
+  expText.style.display = "block";
+}
+
+exp_tile.onmouseout = function() {
+  expText.style.display = "none";
+}
+
+// Skills
+var skillText = document.getElementById('skills_text');
+skillText.style.display = "none";
+var skill_tile = document.getElementById('skills');
+
+skill_tile.onmouseover = function() {
+  skillText.style.display = "block";
+}
+
+skill_tile.onmouseout = function() {
+  skillText.style.display = "none";
+}
+
+// Projects
+var projectText = document.getElementById('projects_text');
+projectText.style.display = "none";
+var project_tile = document.getElementById('projects');
+
+project_tile.onmouseover = function() {
+  projectText.style.display = "block";
+}
+
+project_tile.onmouseout = function() {
+  projectText.style.display = "none";
+}
+
+// Co-curricular Activities
+var extraText = document.getElementById('extra_text');
+extraText.style.display = "none";
+var extra_tile = document.getElementById('extra');
+
+extra_tile.onmouseover = function() {
+  extraText.style.display = "block";
+}
+
+extra_tile.onmouseout = function() {
+  extraText.style.display = "none";
+}
